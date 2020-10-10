ba9e6f4bd85ef0a7e9cbce2718e4218a7f4a656f
Alves <victor.de.s.alves@accenture.com>
2020-10-10 15:16:03 -0300

    first commit

---
+++ app/src/main/java/com/wolfdevelloper/estudo/index/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.wolfdevelloper.estudo.index
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wolfdevelloper.estudo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
